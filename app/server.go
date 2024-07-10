285255f129a64beb5d3a2326139da9625c61bf65
jiahao-shi <jiahao.shi@ui.com>
2024-07-10 15:36:31 +0800

    read requset line & get request target

--- app/server.go
+++ app/server.go
@@ -4,19 +4,45 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 	// Uncomment this block to pass the first stage
 	// "net"
 	// "os"
 )
 
 func handleConnection(conn net.Conn) {
-	_, err := conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-	//_, err := conn.Write([]byte("HTTP/1.1 200 OK\\r\\n\\r\\n"))
+
+	req := make([]byte, 1024)
+	_, err := conn.Read(req)
+	if err != nil {
+		fmt.Printf("conn read failed %v\n", err)
+		return
+	}
+	reqSections := strings.Split(string(req), "\r\n")
+
+	// get request line
+	reqLine := reqSections[0]
+	path := strings.Split(reqLine, " ")[1]
+
+	fmt.Println("path: ", path)
+
+	res := "HTTP/1.1 200 OK\r\n\r\n"
+
+	if path != "/" {
+		res = "HTTP/1.1 404 Not Found\r\n\r\n"
+	}
+	_, err = conn.Write([]byte(res))
 	if err != nil {
 		fmt.Printf("conn write failed %v\n", err)
 		return
 	}
 }
+
+/**
+request line
+HttpMethod  RequestTarget HttpVersion
+
+*/
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.

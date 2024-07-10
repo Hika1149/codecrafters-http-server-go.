48b59a55936576b7d7a5998e32e7f7dfa81a4164
jiahao-shi <jiahao.shi@ui.com>
2024-07-10 19:46:00 +0800

    write respond with body

--- app/server.go
+++ app/server.go
@@ -5,14 +5,26 @@ import (
 	"net"
 	"os"
 	"strings"
-	// Uncomment this block to pass the first stage
-	// "net"
-	// "os"
 )
 
+/*
+*
+Request
+
+	Request line
+		HttpMethod  RequestTarget HttpVersion
+
+Response
+
+	Status line
+		HttpVersion StatusCode ReasonPhrase
+	Header
+		HeaderName: HeaderValue
+	Body
+*/
 func handleConnection(conn net.Conn) {
 
-	req := make([]byte, 1024)
+	req := make([]byte, 1024*5)
 	_, err := conn.Read(req)
 	if err != nil {
 		fmt.Printf("conn read failed %v\n", err)
@@ -26,30 +38,32 @@ func handleConnection(conn net.Conn) {
 
 	fmt.Println("path: ", path)
 
-	res := "HTTP/1.1 200 OK\r\n\r\n"
+	res := make([]byte, 0)
 
-	if path != "/" {
-		res = "HTTP/1.1 404 Not Found\r\n\r\n"
+	if strings.HasPrefix(path, "/echo/") {
+		echo := strings.Split(path, "/echo/")[1]
+		res = append(res, []byte("HTTP/1.1 200 OK\r\n")...)
+		res = append(res, []byte("Content-Type: text/plain\r\n")...)
+		res = append(res, []byte(fmt.Sprintf("Content-Length: %v\r\n", len(echo)))...)
+		res = append(res, []byte("\r\n")...) // End of headers
+		res = append(res, []byte(fmt.Sprintf("%s\r\n", echo))...)
+	} else if path == "/" {
+		res = append(res, []byte("HTTP/1.1 200 OK\r\n\r\n")...)
+	} else {
+		res = append(res, []byte("HTTP/1.1 404 Not Found\r\n\r\n")...)
 	}
-	_, err = conn.Write([]byte(res))
+
+	_, err = conn.Write(res)
 	if err != nil {
 		fmt.Printf("conn write failed %v\n", err)
 		return
 	}
 }
 
-/**
-request line
-HttpMethod  RequestTarget HttpVersion
-
-*/
-
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
-	//
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
@@ -62,7 +76,6 @@ func main() {
 			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
-
 		go handleConnection(conn)
 	}
 

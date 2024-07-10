498666c6a286102a3341caa24289700ac896c600
jiahao-shi <jiahao.shi@ui.com>
2024-07-10 14:22:59 +0800

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -9,6 +9,15 @@ import (
 	// "os"
 )
 
+func handleConnection(conn net.Conn) {
+	_, err := conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	//_, err := conn.Write([]byte("HTTP/1.1 200 OK\\r\\n\\r\\n"))
+	if err != nil {
+		fmt.Printf("conn write failed %v\n", err)
+		return
+	}
+}
+
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
@@ -21,9 +30,14 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		go handleConnection(conn)
 	}
+
 }
